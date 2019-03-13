a9fd2c0d2c1efe16645024509bddf29d1b134e42
heisengit <simone.cristofaro@studenti.unicampania.it>
2019-03-13 16:58:27 +0100

    Commit iniziale

---
+++ app/src/main/java/com/example/supercalc/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.supercalc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+//prova
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val a = 10
+
+
+        //prova
+    }
+}
